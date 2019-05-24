9af7db2c698a8a0f029fbe55c604199465859369
bbeard3 <brett.beard@gtri.gatech.edu>
2019-05-24 14:17:54 -0400

    Initial check-in of Unit tests project.

---
+++ BinaryBaseball.Simulation.Tests/Game/BasesTests.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BinaryBaseball.Simulation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinaryBaseball.Simulation.Tests
+{
+    [TestClass()]
+    public class BasesTests
+    {
+        [TestMethod()]
+        public void BasesTest()
+        {
+            var bases = new Bases();
+
+            // All bases should be null (not occupied by players)
+            foreach (var b in bases.Occupied)
+            {
+                Assert.IsNull(b);
+            }
+        }
+
+        [TestMethod()]
+        public void Bases_EmptyTest()
+        {
+            var bases = new Bases();
+
+            //  Bases should be empty
+            Assert.AreEqual(true, bases.Empty);
+
+            // Put a player on first
+            var player = new BinaryBaseball.Common.Player();
+            bases.Occupied[0] = player;
+
+            // Should NOT be empty
+            Assert.AreEqual(false, bases.Empty);
+        }
+
+        [TestMethod()]
+        public void ClearTest()
+        {
+            var bases = new Bases();
+
+            // Put a player on first
+            var player = new BinaryBaseball.Common.Player();
+            bases.Occupied[0] = player;
+
+            // Clear the bases
+            bases.Clear();
+
+            // All bases should be null (not occupied by players)
+            foreach (var b in bases.Occupied)
+            {
+                Assert.IsNull(b);
+            }
+        }
+    }
+}
---
+++ BinaryBaseball.Simulation.Tests/Game/GameTests.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BinaryBaseball.Simulation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinaryBaseball.Simulation.Tests
+{
+    [TestClass()]
+    public class GameTests
+    {
+        [TestMethod()]
+        public void CalculateLog5Test()
+        {
+            // Arrange
+            var parameters = new GameParameters();
+            var game = new Game(parameters);
+            var privateObject = new PrivateObject(game);
+
+            // Act            
+            object[] args = new object[3] { 0.2122, 0.1784, 0.1559 };
+
+            Double result = (Double)privateObject.Invoke(
+                "CalculateLog5",
+                new Type[] { typeof(Double), typeof(Double), typeof(Double) },
+                args);
+
+            // Assert
+            Assert.AreEqual(0.2405, result, 0.0001);
+        }
+    }
+}
